4e818674a4bfdafaf358c0643eaac52b34912c86
Nary Aina <aa.naryandriambelo@it-students.fr>
2019-05-06 16:25:42 +0200

    Delete Termine

--- Aina_AppliMobile/src/dashboard.js
+++ Aina_AppliMobile/src/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, ScrollView, FlatList, Text, StyleSheet, View } from 'react-native';
+import { TouchableOpacity, TextInput, ScrollView, FlatList, Text, StyleSheet, View } from 'react-native';
 import ModalNewNote from './modalNewNote';
 type Props = {};
 export default class Dashboard extends Component<Props> {
@@ -9,7 +9,12 @@ export default class Dashboard extends Component<Props> {
         notes: [],
         showModalNote: false,
         showNoteView: false,
-        refresh: false
+
+        refresh: false,
+        current_note: '',
+        Update_title: "",
+        Update_description: "",
+        Update_date_update: "",
     }
     componentWillMount() {
         this.GetAllNotes()
@@ -44,66 +49,156 @@ export default class Dashboard extends Component<Props> {
         out = new Date(StringDate).getHours + new Date(StringDate).getMinutes
     }
     notesTouch(lanote) {
-        
+        console.log(lanote)
+        this.setState({
+            showNoteView: true,
+            current_note: lanote
+        })
+    }
+    _onUpdate() {
+        var IPAddr = '10.1.171.68'
+        var idNote = this.state.current_note._id
+        var route = 'http://' + IPAddr + ':3000/notes/' + idNote
+        console.log("la route : " + route)
+        fetch(route, {
+            // fetch('http://http://192.168.33.15:3000/users', {
+            method: 'PUT',
+            credentials: 'same-origin',
+            headers: {
+                'content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: this.state.Update_title,
+                description: this.state.Update_description,
+                date_update: this.state.Update_date_update,
+            }),
+        }).then(function (result) { console.log('result', result); return result.json({}) })
+            .then(function (datas) {
+                this.setState({ showNoteView: false })
+            }
+                .bind(this)
+            );
     }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.headerStyle}>
-                    <TouchableOpacity
-                        onPress={() => this.showModalAdd()}
-                    >
-                        <Text style={styles.menuItemStyle}>
-                            Modal Ici
+    _onDelete() {
+        var IPAddr = '10.1.171.68'
+        var idNote = this.state.current_note._id
+        var route = 'http://' + IPAddr + ':3000/notes/' + idNote
+        fetch(route, {
+            //     // fetch('http://http://192.168.33.15:3000/users', {
+            method: 'DELETE',
+            credentials: 'same-origin',
+            headers: {
+                'content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: this.state.current_note._id
+            }),
+        }).then((response) => response.text())
+            .then((datas) => {
+                this.setState({ showNoteView: false })
+            })
+    }
+
+render() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.headerStyle}>
+                <TouchableOpacity
+                    onPress={() => this.showModalAdd()}
+                >
+                    <Text style={styles.menuItemStyle}>
+                        Ajouter une note
                         </Text>
-                    </TouchableOpacity>
-                </View>
+                </TouchableOpacity>
+            </View>
 
 
-                {(this.state.showModalNote) ? (
-                    <ModalNewNote setParentState={this.setState.bind(this)} />
-                ) : (
-                        <View>
-                            <TouchableOpacity onPress={this.GetAllNotes.bind(this)} style={styles.button1}>
-                                <Text>Rafraichir la liste</Text>
-                            </TouchableOpacity>
-                            <FlatList
-                                data={this.state.notes}
-                                extraData={this.state.refresh}
-                                keyExtractor={(item, index) => index.toString()}
-                                renderItem={({ item }) => (
+            {(this.state.showModalNote) ? (
+                <ModalNewNote setParentState={this.setState.bind(this)} />
+            ) : (
+                    (this.state.showNoteView) ? (
+                        <View style={{ flexDirection: 'column' }}>
+                            <Text>Titre : </Text>
+                            <TextInput
+                                style={styles.inputTitle}
+                                value={this.state.current_note.Update_title}
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                onChangeText={(text) => {
+                                    this.setState({
+                                        Update_title: text,
+                                        Update_date_update: new Date().toString()
+                                    })
+                                }} />
+                            <Text>Contenu : </Text>
+                            <TextInput
+                                style={styles.inputContent}
+                                value={this.state.current_note.Update_description}
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                onChangeText={(text) => {
+                                    this.setState({
+                                        Update_description: text,
+                                        Update_date_update: new Date().toString()
+                                    })
+                                }} />
+                            <View style={styles.buttonView}>
+                                <TouchableOpacity onPress={this._onUpdate.bind(this)} style={styles.button1}>
+                                    <Text>Modifier la note</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={this._onDelete.bind(this)} style={styles.buttonRed2}>
+                                    <Text>Effacer la note</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => {
+                                    this.setState({ showNoteView: false })
+                                }}
+                                    style={styles.button3}>
+                                    <Text>Retour</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    ) : (
+                            <View>
+                                <TouchableOpacity onPress={this.GetAllNotes.bind(this)} style={styles.button1}>
+                                    <Text>Rafraichir la liste</Text>
+                                </TouchableOpacity>
+                                <FlatList
+                                    data={this.state.notes}
+                                    extraData={this.state.refresh}
+                                    keyExtractor={(item, index) => index.toString()}
+                                    renderItem={({ item }) => (
 
-                                    <TouchableOpacity
-                                        style={styles.listStyle}
-                                        onPress={() => this.notesTouch(item)}>
-                                        <View >
-                                            <View style={{ flexDirection: 'column', flexShrink: 1 }}>
-                                                <Text>{item.title}</Text>
-                                                <Text style={{ flex: 1, flexWrap: 'wrap' }}>{item.description}</Text>
-                                                {/* <Text>Date de creation : 
+                                        <TouchableOpacity
+                                            style={styles.listStyle}
+                                            onPress={() => this.notesTouch(item)}>
+                                            <View >
+                                                <View style={{ flexDirection: 'column', flexShrink: 1 }}>
+                                                    <Text>{item.title}</Text>
+                                                    <Text style={{ flex: 1, flexWrap: 'wrap' }}>{item.description}</Text>
+                                                    {/* <Text>Date de creation : 
                                             {new Date(item.date_create).getHours()}
                                             h
                                             {new Date(item.date_create).getMinutes()}
                                             </Text> */}
-                                                <Text>Date de modification :
-                                                    {new Date(item.date_update).getHours()}h{new Date(item.date_update).getMinutes()}
-                                                     {/* le {new Date(item.date_update).getDate()}/{new Date(item.date_update).getMonth()}/{new Date(item.date_update).getFullYear()} */}
-                                                </Text>
+                                                    <Text>Date de modification :
+                                                    {new Date(item.date_update).getHours()}h{new Date(item.date_update).getMinutes()}_
+                                                     le {new Date(item.date_update).getDate()}/{new Date(item.date_update).getMonth()}/{new Date(item.date_update).getFullYear()}
+                                                    </Text>
+                                                </View>
                                             </View>
-                                        </View>
-                                    </TouchableOpacity>
-                                )}>
-                            </FlatList>
-                        </View>
-                    )}
-            </View>
-        )
-    }
+                                        </TouchableOpacity>
+                                    )}>
+                                </FlatList>
+                            </View>
+                        )
+                )}
+        </View>
+    )
+}
 }
 const styles = StyleSheet.create({
     container: {
-
         flexDirection: 'column'
     },
     listStyle: {
@@ -123,11 +218,48 @@ const styles = StyleSheet.create({
     menuItemStyle: {
         marginLeft: 'auto',
         backgroundColor: 'red'
-    },    
+    },
     button1: {
         backgroundColor: '#1e90ff',
         padding: 10,
         margin: 5,
         borderRadius: 5,
+    },
+    buttonView: {
+        margin: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+
+    buttonRed2: {
+        backgroundColor: '#DC143C',
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+    },
+    button3: {
+        backgroundColor: 'green',
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+    },
+    noteView: {
+        flexDirection: 'column',
+        backgroundColor: 'red',
+        flex: 1,
+    },
+    inputTitle: {
+        height: 40,
+        borderColor: 'silver',
+        borderWidth: 1,
+        margin: 10
+    },
+    inputContent: {
+        height: 200,
+
+        borderColor: 'silver',
+        borderWidth: 1,
+        margin: 10,
+        padding: 0
     },
 });
--- README_Aina.txt
+++ README_Aina.txt
@@ -2,5 +2,5 @@ Pour pouvoir tester en local, j'ai du utiliser l'adress IP de la machine dans l'
 un "localhost" ne fonctionnant pas.
 L'adresse IP de la machine est 10.1.171.68
 
-- Dans DashBoard la fonction GetAllNotes()
+- Dans DashBoard la fonction GetAllNotes(),_onUpdate(),_onDelete
 - Dans modalNewNote la fonction _onPost()
